8dc16cf5b14b204c21209eb24e8c9890d6bfb6ab
Zeus30007 <130933523+Zeus30007@users.noreply.github.com>
2023-11-28 16:33:48 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,25 +10,25 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "16028428623"
-global.ownername = "god"
+global.ownernomer = "6797309646"
+global.ownername = "ɢʀᴇᴇᴋ"
 global.ytname = "YT: NASSWORKS"
 global.socialm = "Github: NASS-WORKS"
-global.location = "NIGERIA,FCT"
+global.location = "Japan, akina"
 
-global.ownernumber = '16028428623'  //creator number
-global.ownername = 'god' //owner name
-global.botname = 'BOT:ⁿNASS-WORKS' //name of the bot
+global.ownernumber = '6797309646'  //creator number
+global.ownername = 'ɢʀᴇᴇᴋ' //owner name
+global.botname = 'BOT:ⁿɢʀᴇᴇᴋ' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
-global.author = 'NASS-WORKS\n\nContact: +2347069096801'
+global.packname = 'steal it its part of lord'
+global.author = 'ɢʀᴇᴇᴋ👤'
 //console view/theme
-global.themeemoji = '🪀'
-global.wm = "NASS-WORKS Bot Inc."
+global.themeemoji = '👤'
+global.wm = "ɢʀᴇᴇᴋ Bot Inc."
 
 //theme link
-global.link = 'wa.me//2347069096801'
+global.link = 'wa.me//6797309646'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
@@ -40,25 +40,25 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
 //v2 is link + image menu,
 //v3 is video menu,
 //v4 is call end menu
-global.typemenu = 'v2'
+global.typemenu = 'v3'
 
 //reply messages
 global.mess = {
-    done: 'Done !',
-    prem: 'This feature can be used by premium user only',
-    admin: 'This feature can be used by admin only',
-    botAdmin: 'This feature can only be used when the bot is a group admin ',
-    owner: 'This feature can be used by owner only',
+    done: 'Approved',
+    prem: 'This is restricted to peasants like you only premium users are allowed',
+    admin: 'This feature is also restricted to peasants like you only the superior admins can use it',
+    botAdmin: 'This feature can only be used when the bot is not a peasant but superior admin',
+    owner: 'This feature can be used by my most superior masters lord ɢʀᴇᴇᴋ his wife, daughter and lord NATHAN only',
     group: 'This feature is only for groups',
     private: 'This feature is only for private chats',
-    wait: 'In process... ',    
+    wait: 'hold your horses... ',    
     error: 'Error!',
 }
 
